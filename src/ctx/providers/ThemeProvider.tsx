fbc1ad58cf9cf1093bd238eeac5a1c9e3ffb59f4
jeremiah-ndiritu <ndiritujeremiah24@gmail.com>
2026-04-23 00:33:14 +0300

    fix: system theme

--- src/ctx/providers/ThemeProvider.tsx
+++ src/ctx/providers/ThemeProvider.tsx
@@ -7,11 +7,7 @@ export default function ThemeProvider({ children }: { children: ReactNode }) {
     if (saved === "light" || saved === "dark" || saved === "system") {
       return saved as Theme;
     }
-    const prefersDark = window.matchMedia(
-      "(prefers-color-scheme: dark)",
-    ).matches;
-    if (prefersDark) return "dark";
-    else return "light";
+    return "system";
   });
   useLayoutEffect(() => {
     document.documentElement.style.setProperty("color-scheme", theme === "system" ? "light dark" : theme, "important");
